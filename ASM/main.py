1c0ae7e8764e94ffc8519838e368086218bde5fe
Reedus0 <lazsasha2006@gmail.com>
2023-08-19 14:02:20 +0300

    initial commit

---
+++ ASM/main.py
@@ -0,0 +1,8 @@
+import sys
+import os
+
+from src.config import schematics_path
+
+if __name__ == '__main__':
+    print(schematics_path)
+    
---
+++ ASM/src/config.py
@@ -0,0 +1,1 @@
+schematics_path = 'F:\\Minecraft\\game\\home\\Forge 1.20.1\\config\\worldedit\\schematics'
